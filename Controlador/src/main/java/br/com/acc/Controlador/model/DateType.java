dcbdc337b1307b6690c46090cd434802cca66b34
VitorInc <vitordc.contato@gmail.com>
2021-07-18 00:48:13 -0300

    Created DateType and add atributes

---
+++ Controlador/src/main/java/br/com/acc/Controlador/model/DateType.java
@@ -0,0 +1,14 @@
+package br.com.acc.Controlador.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class DateType {
+    private long id;
+    private String decription;
+}
